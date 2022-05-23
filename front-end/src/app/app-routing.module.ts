f59ad679c2cb4776d79daf5e118791188c5b8384
Zakaria Dani <zakaria.dani2015@gmail.com>
2022-05-23 20:11:44 +0100

    add-login-regiter-pages

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: WelcomePageComponent
+  },
+  {
+    path: "login",
+    component: LoginPageComponent
+  },
+  {
+    path:"register",
+    component: RegisterPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
