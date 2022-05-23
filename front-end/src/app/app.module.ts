e80adcb91d5e3a7eff1fb1f048d2379594a64e3f
Zakaria Dani <zakaria.dani2015@gmail.com>
2022-05-23 17:19:02 +0100

    first commit

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomePageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
