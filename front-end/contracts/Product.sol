e24e9fa19e70e757253d6829aee3d470e10c0f87
aymane-daif <aymaned345@gmail.com>
2022-06-16 13:52:27 +0100

    :sparkles: add Product contract

--- front-end/contracts/Product.sol
+++ front-end/contracts/Product.sol
@@ -7,4 +7,48 @@ contract Product {
     function products() external view returns (uint) {
         return max_products;
     }
+    
+    ProductItem[] public productItems;
+
+    struct ProductItem{
+        address creator;
+        string productName;
+        uint256 productId;
+        string date;
+    }
+    
+    
+    
+    function addProductItem(string memory _text, string memory _date) public returns (bool) {
+        ProductItem memory newItem = ProductItem({creator: msg.sender, productName: _text, productId: items, date: _date});
+        productItems.push(newItem);
+        return true;
+    }
+
+    function getProductItems() constant returns (uint256[], string[]) {
+        uint length = productItems.length;
+
+        uint256[] memory ids = new uint256[](length);
+        string[] memory productNames = new string[](length);
+
+        for (uint i = 0; i < length; i++) {
+            ids[i] = productItems[i].productId;
+            productNames[i] = productItems[i].productName;
+        }
+
+        return (ids, productNames);
+    }
+    
+    function deleteProductItem(uint index) returns (bool success) {
+        if (index >= productItems.length) return;
+
+        for (uint i = index; i < productItems.length - 1; i++){
+            productItems[i] = productItems[i+1];
+        }
+
+        delete productItems[productItems.length - 1];
+        productItems.length--;
+        return true;
+    }
+    
 }
