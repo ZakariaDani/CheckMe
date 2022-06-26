5f9e60aab24530f12bac0b3be7ac4f68c874201a
aymane-daif <aymaned345@gmail.com>
2022-06-26 16:29:37 +0100

    :sparkles: add product's details

--- front-end/src/app/check-product/check-product.component.ts
+++ front-end/src/app/check-product/check-product.component.ts
@@ -12,6 +12,12 @@ export class CheckProductComponent implements OnInit {
   constructor(private productService: ProductService) {}
   message = '';
   isMessageShown = false;
+  productDetails: any = {
+    creator: '',
+    productName: '',
+    price: null,
+    dateCreation: '',
+  };
   ngOnInit(): void {}
 
   async checkProduct(checkProductForm: NgForm) {
@@ -22,9 +28,14 @@ export class CheckProductComponent implements OnInit {
 
     let result = await this.productService.checkProduct(productId);
     if (result == 'fake') {
-      this.message = 'This product is fake';
+      this.message = 'This product is fake ❌';
     } else {
-      this.message = 'This product is real';
+      this.message = 'This product is real ✅';
+      console.log(result);
+      this.productDetails.creator = result.username;
+      this.productDetails.productName = result.productName;
+      this.productDetails.price = result.price;
+      this.productDetails.dateCreation = result.date;
     }
     this.isMessageShown = true;
     checkProductForm.reset();
